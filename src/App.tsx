1f8850e675f7e318973689d9e6f24d86af326b02
Refilwe15 <mrefilwe880@gmail.com>
2025-09-09 14:21:42 +0200

    first page completed

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Form from './components/Login.tsx'
+import './styles/global.css'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Form label={''} placeholder={''} />
+    </>
+  )
+}
+
+export default App
